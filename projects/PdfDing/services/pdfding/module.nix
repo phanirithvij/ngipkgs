d64f302d0d9f02f88576cb3d84af90dc2fc911c8
phanirithvij <phanirithvij2000@gmail.com>
2025-11-07 23:57:30 +0530

    WIP projects/pdfding: init
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ projects/PdfDing/services/pdfding/module.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.services.pdfding;
+in
+{
+  options.services.pdfding = {
+    enable = lib.mkEnableOption "pdfding";
+    # TODO check if this makes sense for ngipkgs
+    # original comment in the template says leave it if your package is not in nixpkgs
+    package = lib.mkPackageOption pkgs "pdfding" { };
+  };
+  config = lib.mkIf cfg.enable {
+    # TODO systemd service based on some existing django service in nixpgks
+    # It should allow managing via manage.py
+  };
+}
