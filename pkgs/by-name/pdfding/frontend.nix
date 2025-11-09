9ff52020f2fe37fbe79a9277ac6291191171fa45
phanirithvij <phanirithvij2000@gmail.com>
2025-11-09 18:48:52 +0530

    WIP tmpsave
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ pkgs/by-name/pdfding/frontend.nix
@@ -0,0 +1,74 @@
+{
+  lib,
+  stdenv,
+  fetchzip,
+  fetchFromGitHub,
+  nodejs,
+  npmHooks,
+  fetchNpmDeps,
+  tailwindcss_4,
+  moreutils,
+  jq,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "pdfding-frontend";
+  version = "1.3.3";
+  src = fetchFromGitHub {
+    owner = "mrmn2";
+    repo = "PdfDing";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-TRQQdZa4X+Kx13QCYChqkN4eT5VJjAti+DR+MqOPsOU=";
+  };
+
+  npmDeps = fetchNpmDeps {
+    inherit (finalAttrs) src;
+    name = "pdfding-frontend-${finalAttrs.version}-npm-deps";
+    hash = "sha256-m9zr6+3LHG40dDFfTBXwqHCJVyTGuurNJq7xRrosFlA=";
+  };
+
+  # npm error Invalid package, must have name and version
+  postPatch = ''
+    ${lib.getExe jq} '. += { "name": "pdfding-frontend", "version": "${finalAttrs.version}" }' package.json \
+       | ${lib.getExe' moreutils "sponge"} package.json
+  '';
+
+  # TODO pdfjs comes with js source maps, should they be removed in postFetch?
+  pdfjs =
+    let
+      # version from pdfding dockerfile
+      # TODO handle in updateScript
+      pdfjsVersion = "5.4.149";
+    in
+    fetchzip {
+      url = "https://github.com/mozilla/pdf.js/releases/download/v${pdfjsVersion}/pdfjs-${pdfjsVersion}-dist.zip";
+      hash = "sha256-f/wdLva8bsMwcETlT1LiFblbOXbDAOFOiPvpJ6Ziysk=";
+      stripRoot = false;
+    };
+
+  nativeBuildInputs = [
+    nodejs
+    npmHooks.npmConfigHook
+    # it is in package.json and thus node_modules but no cli executable
+    tailwindcss_4
+  ];
+
+  # keeping the file structure same as upstream to minimise confusion
+  buildPhase = ''
+    runHook preBuild
+    mkdir -p $out/pdfding
+    cp -r --no-preserve=mode pdfding/static $out/pdfding/static
+    cp -r --no-preserve=mode $pdfjs $out/pdfding/static/pdfjs
+
+    tailwindcss -i $out/pdfding/static/css/input.css -o $out/pdfding/static/css/tailwind.css --minify
+    rm $out/pdfding/static/css/input.css
+
+    for i in build/pdf.mjs build/pdf.sandbox.mjs build/pdf.worker.mjs web/viewer.mjs; \
+    do node_modules/terser/bin/terser $out/pdfding/static/pdfjs/$i --compress -o $out/pdfding/static/pdfjs/$i; done
+
+    npm run build
+
+    cp -r pdfding/static/js $out/pdfding/static
+
+    runHook postBuild
+  '';
+})
--- pkgs/by-name/pdfding/package.nix
+++ pkgs/by-name/pdfding/package.nix
@@ -1,18 +1,15 @@
 {
   lib,
-  fetchzip,
-  fetchFromGitHub,
   stdenv,
-  python3,
-
-  nodejs,
-  npmHooks,
-  fetchNpmDeps,
-  tailwindcss_4,
-  moreutils,
-  jq,
+  python313,
+  callPackage,
+  fetchFromGitHub,
+  makeWrapper,
 }:
 /*
+  - pretalx seems to be a django application in ngipkgs
+  - paperless-ngx
+  - glitchtip
   - package
     - [x] frontend
     - [ ] server
@@ -22,69 +19,7 @@
   - [ ] example
 */
 let
-  version = "1.3.3";
-  src = fetchFromGitHub {
-    owner = "mrmn2";
-    repo = "PdfDing";
-    tag = "v${version}";
-    hash = "sha256-TRQQdZa4X+Kx13QCYChqkN4eT5VJjAti+DR+MqOPsOU=";
-  };
-  frontend = stdenv.mkDerivation {
-    pname = "pdfding-frontend";
-    inherit version src;
-
-    npmDeps = fetchNpmDeps {
-      inherit src;
-      name = "pdfding-frontend-${version}-npm-deps";
-      hash = "sha256-m9zr6+3LHG40dDFfTBXwqHCJVyTGuurNJq7xRrosFlA=";
-    };
-
-    # npm error Invalid package, must have name and version
-    postPatch = ''
-      ${lib.getExe jq} '. += { "name": "pdfding-frontend", "version": "${version}" }' package.json \
-         | ${lib.getExe' moreutils "sponge"} package.json
-    '';
-
-    # TODO pdfjs comes with js source maps, should they be removed in postFetch?
-    pdfjs =
-      let
-        # version from pdfding dockerfile
-        # TODO handle in updateScript
-        pdfjsVersion = "5.4.149";
-      in
-      fetchzip {
-        url = "https://github.com/mozilla/pdf.js/releases/download/v${pdfjsVersion}/pdfjs-${pdfjsVersion}-dist.zip";
-        hash = "sha256-f/wdLva8bsMwcETlT1LiFblbOXbDAOFOiPvpJ6Ziysk=";
-        stripRoot = false;
-      };
-
-    nativeBuildInputs = [
-      nodejs
-      npmHooks.npmConfigHook
-      # it is in package.json and thus node_modules but no cli executable
-      tailwindcss_4
-    ];
-
-    # keeping the file structure same as upstream to minimise confusion
-    buildPhase = ''
-      runHook preBuild
-      mkdir -p $out/pdfding
-      cp -r --no-preserve=mode pdfding/static $out/pdfding/static
-      cp -r --no-preserve=mode $pdfjs $out/pdfding/static/pdfjs
-
-      tailwindcss -i $out/pdfding/static/css/input.css -o $out/pdfding/static/css/tailwind.css --minify
-      rm $out/pdfding/static/css/input.css
-
-      for i in build/pdf.mjs build/pdf.sandbox.mjs build/pdf.worker.mjs web/viewer.mjs; \
-      do node_modules/terser/bin/terser $out/pdfding/static/pdfjs/$i --compress -o $out/pdfding/static/pdfjs/$i; done
-
-      npm run build
-
-      cp -r pdfding/static/js $out/pdfding/static
-
-      runHook postBuild
-    '';
-  };
+  python3 = python313;
   python = python3.override {
     self = python;
     packageOverrides = final: prev: {
@@ -92,23 +27,15 @@ let
       django = prev.django_5_2;
     };
   };
-in
-python.pkgs.buildPythonApplication rec {
-  pname = "pdfding";
-  inherit version src;
-  pyproject = true;
 
-  build-system = with python.pkgs; [ poetry-core ];
-
-  dependencies = with python.pkgs; [
+  pythonPackages = with python.pkgs; [
     django
     django-allauth
     django-cleanup
     django-htmx
     gunicorn
-    huey # TODO what's run_huey in django
     markdown
-    minio # TODO is it still foss?
+    minio
     nh3
     psycopg2-binary
     pypdf
@@ -117,22 +44,87 @@ python.pkgs.buildPythonApplication rec {
     qrcode
     rapidfuzz
     ruamel-yaml
-    supervisor # TODO what about this? does it work with systemd service?
     whitenoise
+
+    # collectstatic needs these
+    requests
+    pyjwt
+    cryptography
+
+    huey # TODO what's run_huey in django
+    supervisor # TODO what about this? does it work with systemd service?
+
+    poetry-core
   ];
 
-  preBuild = ''
+  frontend = callPackage ./frontend.nix { };
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "pdfding";
+  version = "1.3.3";
+  src = fetchFromGitHub {
+    owner = "mrmn2";
+    repo = "PdfDing";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-TRQQdZa4X+Kx13QCYChqkN4eT5VJjAti+DR+MqOPsOU=";
+  };
+  pyproject = true;
+
+  propagatedBuildInputs = pythonPackages;
+
+  nativeBuildInputs = [
+    python
+    makeWrapper
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+
     # prod build, as per dockerfile
     rm pdfding/core/settings/dev.py
 
+    # remove originals, copy from frontend
     rm -rf pdfding/static
-    # can't do symlinking because python package doesn't allow it
-    cp -r --no-preserve=mode ${passthru.frontend}/pdfding/static pdfding/static
+    ln -s ${finalAttrs.passthru.frontend}/pdfding/static pdfding/static
+
+    ${python.pythonOnBuildForHost.interpreter} pdfding/manage.py collectstatic
+
+    # not needed, now we have staticfiles directory
+    rm -rf pdfding/static
+
+    # remove django md5 hash from filenames of pdfjs as it will mess up the relative imports because of the whitenoise setup
+    sh -x \
+        && export PDFJS_PATH="pdfding/staticfiles/pdfjs" \
+        && for file_name in $(find $PDFJS_PATH -type f -not -path "$PDFJS_PATH/web/images/*");  \
+           do \
+                if [[ $file_name =~ "LICENSE" ]]; then \
+                  new=$(echo "$file_name" | sed -E "s/LICENSE\.[a-zA-Z0-9]{12}/LICENSE/"); \
+                else \
+                  new=$(echo "$file_name" | sed -E "s/\.[a-zA-Z0-9]{12}\./\./"); \
+                fi; \
+                mv -- "$file_name" "$new"; \
+           done \
+        && echo 'Successfully removed hash from pdfjs files'
+
+    echo "VERSION = '${finalAttrs.version}'" > pdfding/core/settings/version.py;
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib
+    cp -r . $out/lib
+    makeWrapper $out/lib/pdfding/manage.py $out/bin/pdfding-manage \
+      --prefix PYTHONPATH : "$PYTHONPATH"
+
+    runHook postInstall
   '';
 
   # TODO too many mismatched deps from project's requirements, and no better solution
   # if some dep doesn't work it needs to be manually overriden via python3.override, packageOverrides
-  pythonRelaxDeps = true;
+  # pythonRelaxDeps = true;
   /*
     pythonRelaxDeps = [
       "django"
@@ -146,10 +138,6 @@ python.pkgs.buildPythonApplication rec {
     ];
   */
 
-  pythonImportsCheck = [
-    "pdfding"
-  ];
-
   passthru = {
     updateScript = ""; # TODO custom update script maybe, for handling npmDeps hash
     inherit frontend; # would allow easily overriding it
@@ -158,10 +146,10 @@ python.pkgs.buildPythonApplication rec {
   meta = {
     description = "Selfhosted PDF manager, viewer and editor offering a seamless user experience on multiple devices";
     homepage = "https://github.com/mrmn2/PdfDing";
-    changelog = "https://github.com/mrmn2/PdfDing/blob/${src.tag}/CHANGELOG.md";
+    changelog = "https://github.com/mrmn2/PdfDing/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.agpl3Only;
     maintainers = with lib.maintainers; [ phanirithvij ];
     teams = with lib.teams; [ ngi ];
-    mainProgram = "pdfding";
+    mainProgram = "pdfding-manage";
   };
-}
+})
