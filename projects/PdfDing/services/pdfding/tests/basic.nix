766750b91c217dc4e0c712fd2fb72cce721e00b2
phanirithvij <phanirithvij2000@gmail.com>
2025-11-13 13:37:01 +0530

    WIP some module
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- projects/PdfDing/services/pdfding/tests/basic.nix
+++ projects/PdfDing/services/pdfding/tests/basic.nix
@@ -1,10 +1,11 @@
 {
+  lib,
   sources,
   ...
 }:
 
 {
-  name = "PdfDing";
+  name = "PdfDing sqlite";
 
   nodes = {
     machine =
@@ -13,10 +14,45 @@
         imports = [
           sources.modules.ngipkgs
           sources.modules.services.pdfding
-          sources.examples.PdfDing."Enable PdfDing"
+          sources.examples.PdfDing.basic
+          "${sources.inputs.sops-nix}/modules/sops"
         ];
+
+        sops = lib.mkForce {
+          age.keyFile = "/run/keys.txt";
+          defaultSopsFile = ./sops/pdfding.yaml;
+        };
+
+        # must run before sops sets up keys
+        boot.initrd.postDeviceCommands = ''
+          cp -r ${./sops/keys.txt} /run/keys.txt
+          chmod -R 700 /run/keys.txt
+        '';
       };
   };
+
+  # Debug interactively with:
+  # - nix run .#checks.x86_64-linux.projects/PdfDing/nixos/tests/basic.driverInteractive -L
+  # - start_all() / run_tests()
+  interactive.sshBackdoor.enable = true; # ssh -o User=root vsock/3
+  interactive.nodes.machine =
+    { config, ... }:
+    {
+      # forward ports from VM to host
+      virtualisation.forwardPorts =
+        map
+          (port: {
+            from = "host";
+            host.port = port;
+            guest.port = port;
+          })
+          [
+            config.services.pdfding.port
+          ];
+
+      # forwarded ports need to be accessible
+      networking.firewall.allowedTCPPorts = [ config.services.pdfding.port ];
+    };
 
   # TODO
   # Tests the most basic user functionality expected from pdfding
@@ -36,6 +72,6 @@
       # make user consume pdfs via admin
       # test if user can access via API
 
-      machine.succeed()
+      machine.succeed("ls")
     '';
 }
