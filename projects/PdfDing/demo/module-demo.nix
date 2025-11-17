6ca3aa702a1e743a6c58f343741ca63c76b8bc1a
phanirithvij <phanirithvij2000@gmail.com>
2025-11-17 23:26:13 +0530

    projects/pdfding: init demo
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- projects/PdfDing/demo/module-demo.nix
+++ projects/PdfDing/demo/module-demo.nix
@@ -8,9 +8,6 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    # demo mode has some restrictions, it is supposed to be a public instance demo
-    # demo for ngipkgs should be a full instance demo without restrictions
-    # services.pdfding.extraEnvironment.DEMO_MODE = "TRUE";
     programs.bash.interactiveShellInit = ''
       echo "PdfDing is starting. Please wait ..."
       until systemctl show pdfding.service | grep -q ActiveState=active; do sleep 1; done
