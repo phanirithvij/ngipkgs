20fb3ed9e1bc67c8187483485b8431a1852cc145
phanirithvij <phanirithvij2000@gmail.com>
2025-11-17 23:26:26 +0530

    projects/pdfding: init examples
    Co-authored-by: eljamm <fedi.jamoussi@protonmail.ch>
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- projects/PdfDing/services/pdfding/examples/postgres.nix
+++ projects/PdfDing/services/pdfding/examples/postgres.nix
@@ -1,23 +1,8 @@
 { config, ... }:
 {
-  sops = {
-    # See <https://github.com/Mic92/sops-nix>.
-    age.keyFile = "/dev/null"; # For a production configuration, set this option.
-    defaultSopsFile = "/dev/null"; # For a production configuration, set this option.
-    validateSopsFiles = false; # For a production configuration, remove this line.
-    secrets."pdfding/django/secret_key_file" = {
-      owner = config.services.pdfding.user;
-      group = config.services.pdfding.group;
-    };
-    secrets."pdfding/database/password" = {
-      owner = config.services.pdfding.user;
-      group = config.services.pdfding.group;
-    };
-  };
-
   services.pdfding = {
     enable = true;
-    secretKeyFile = config.sops.secrets."pdfding/django/secret_key_file".path;
+    secretKeyFile = config.sops.secrets."pdfding/django/secret_key".path;
     database.createLocally = true;
     database.type = "postgres";
     database.passwordFile = config.sops.secrets."pdfding/database/password".path;
@@ -29,7 +14,19 @@
     };
   };
 
-  # TODO email is being sent to /var/mail and pdfding logs it to console
-  # https://github.com/mrmn2/PdfDing/issues/110
-  # there is an smtp setup available, need to test it via running some smtpserver
+  # Secrets management
+  # See <https://github.com/Mic92/sops-nix>
+  sops = {
+    age.keyFile = "/dev/null"; # For a production configuration, set this option.
+    defaultSopsFile = "/dev/null"; # For a production configuration, set this option.
+    validateSopsFiles = false; # For a production configuration, remove this line.
+    secrets."pdfding/django/secret_key" = {
+      owner = config.services.pdfding.user;
+      group = config.services.pdfding.group;
+    };
+    secrets."pdfding/database/password" = {
+      owner = config.services.pdfding.user;
+      group = config.services.pdfding.group;
+    };
+  };
 }
