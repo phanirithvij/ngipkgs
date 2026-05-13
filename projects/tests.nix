5e0c2ec540b80ef9fa6394f8fb60d738872d1b20
phanirithvij <phanirithvij2000@gmail.com>
2026-05-13 16:55:46 +0530

    option removed
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- projects/tests.nix
+++ projects/tests.nix
@@ -24,7 +24,6 @@ let
           # kmscon allows zooming with [Ctrl] + [+] and [Ctrl] + [-]
           services.kmscon = {
             enable = true;
-            autologinUser = "root";
             fonts = [
               {
                 name = "Hack";
